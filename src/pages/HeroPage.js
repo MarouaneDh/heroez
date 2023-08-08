16158001f30e39e4594c6c455f6acd1bc87a709b
Marouane Dhambri <marouane.dhambri@agence-inspire.com>
2023-08-08 14:47:22 +0100

    integrated blurred card

--- src/pages/HeroPage.js
+++ src/pages/HeroPage.js
@@ -36,38 +36,43 @@ const HeroPage = () => {
 
     return (
         hero && <div className="hero-page-container">
-            <img className="hero-page-image" alt={hero.name} src={hero.image.url} />
-            <div className="hero-details">
-                <div>
-                    {
-                        hero.biography.alignment === 'good' ?
-                            <GiAngelOutfit className="good" /> :
-                            <GiDevilMask className="evil" />
-                    }
-                    <h1>{hero?.name}</h1>
-                </div>
-                <div className="hero-physique">
-                    <span>{hero.appearance.gender}{hero.appearance.race !== "null" ? " " + hero.appearance.race : ''}, {hero.appearance["hair-color"]}-haired and {hero.appearance["eye-color"]}-eyed </span>
+            <div class="card">
+                <img className="img" alt={hero.name} src={hero.image.url} />
+                <div class="textBox">
+                    <div className="hero-details">
+                        <div>
+                            {
+                                hero.biography.alignment === 'good' ?
+                                    <GiAngelOutfit className="good" /> :
+                                    <GiDevilMask className="evil" />
+                            }
+                            <h1>{hero?.name}</h1>
+                        </div>
+                        <div className="hero-physique">
+                            <span>{hero.appearance.gender}{hero.appearance.race !== "null" ? " " + hero.appearance.race : ''}, {hero.appearance["hair-color"]}-haired and {hero.appearance["eye-color"]}-eyed </span>
 
-                    <div>
-                        <GiBodyHeight />
-                        <span>{hero.appearance.height[1]} ({hero.appearance.height[0]})</span>
+                            <div>
+                                <GiBodyHeight />
+                                <span>{hero.appearance.height[1]} ({hero.appearance.height[0]})</span>
+                            </div>
+                            <div>
+                                <GiWeight />
+                                <span>{hero.appearance.weight[1]} ({hero.appearance.weight[0]})</span>
+                            </div>
+                        </div>
+                        <div>
+                            <span>Combat : {displayStats(hero.powerstats?.combat)}</span>
+                            <span>Durability : {displayStats(hero.powerstats?.durability)}</span>
+                            <span>Intelligence : {displayStats(hero.powerstats?.intelligence)}</span>
+                            <span>Power : {displayStats(hero.powerstats?.power)}</span>
+                            <span>Speed : {displayStats(hero.powerstats?.speed)}</span>
+                            <span>Strength : {displayStats(hero.powerstats?.strength)}</span>
+                        </div>
                     </div>
-                    <div>
-                        <GiWeight />
-                        <span>{hero.appearance.weight[1]} ({hero.appearance.weight[0]})</span>
-                    </div>
-                </div>
-                <div>
-                    <span>Combat : {displayStats(hero.powerstats?.combat)}</span>
-                    <span>Durability : {displayStats(hero.powerstats?.durability)}</span>
-                    <span>Intelligence : {displayStats(hero.powerstats?.intelligence)}</span>
-                    <span>Power : {displayStats(hero.powerstats?.power)}</span>
-                    <span>Speed : {displayStats(hero.powerstats?.speed)}</span>
-                    <span>Strength : {displayStats(hero.powerstats?.strength)}</span>
                 </div>
             </div>
         </div>
+
     )
 }
 
